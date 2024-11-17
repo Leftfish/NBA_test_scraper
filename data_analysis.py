da26600ca5d09dd643d15a0e43c187448c2ccea8
Leftfish <bohdan@widla.net>
2024-11-17 23:32:49 +0100

    Add very ugly additional stats.

--- data_analysis.py
+++ data_analysis.py
@@ -12,7 +12,16 @@ def main(season):
             'win_few_3pm_more_ftm': 0,
             'win_more_fta': 0,
             'win_more_ftm': 0,
-            'total_games': 0}
+            'total_games': 0,
+            'biggest_win_margin_fewer_3pm': 0,
+            'biggest_win_margin_fewer_3pm_gm': None,
+            'fewest_3pm_win': float('inf'),
+            'fewest_3pm_win_gm': None,
+            'most_3pm_loss': 0,
+            'most_3pm_loss_gm': None,
+            'more_3pm_but_loss': 0,
+            'more_3pm_but_loss_gm': None,
+            }
 
     for game_id in game_data:
         game = game_data[game_id]
@@ -27,6 +36,45 @@ def main(season):
             away_fta = int(game['detailed_away']['fta'])
             away_ftm = int(game['detailed_away']['ft'])
 
+            # biggest win margin with fewer 3PM
+            # home wins but fewer 3PM or away wins but fewer 3PM, points compared
+            win_margin_fewer_3pm = None
+            if home_win and home_3pm < away_3pm:
+                win_margin_fewer_3pm = int(game['home_score']) - int(game['away_score'])
+            elif not home_win and away_3pm < home_3pm:
+                win_margin_fewer_3pm = int(game['away_score']) - int(game['home_score'])
+            if win_margin_fewer_3pm is not None and abs(win_margin_fewer_3pm) > stats['biggest_win_margin_fewer_3pm']:
+                stats['biggest_win_margin_fewer_3pm'] = win_margin_fewer_3pm
+                stats['biggest_win_margin_fewer_3pm_gm'] = game
+
+            # best 3PM margin in a loss
+            # home wins but fewer 3PM or away wins but fewer 3PM, 3PM compared
+            margin_3pt = None
+            if home_win and home_3pm < away_3pm:
+                margin_3pt = abs(away_3pm - home_3pm)
+            elif not home_win and away_3pm < home_3pm:
+                margin_3pt = abs(home_3pm - away_3pm)
+            if margin_3pt is not None and margin_3pt > stats['more_3pm_but_loss']:
+                stats['more_3pm_but_loss'] = margin_3pt
+                stats['more_3pm_but_loss_gm'] = game
+
+            # fewest 3PM in a win
+            if home_win and home_3pm < stats['fewest_3pm_win']:
+                stats['fewest_3pm_win'] = home_3pm
+                stats['fewest_3pm_win_gm'] = game
+            elif not home_win and away_3pm < stats['fewest_3pm_win']:
+                stats['fewest_3pm_win'] = away_3pm
+                stats['fewest_3pm_win_gm'] = game
+
+            # most 3PM in a loss
+            if home_win and away_3pm > stats['most_3pm_loss']:
+                stats['most_3pm_loss'] = away_3pm
+                stats['most_3pm_loss_gm'] = game
+            elif not home_win and home_3pm > stats['most_3pm_loss']:
+                stats['most_3pm_loss'] = home_3pm
+                stats['most_3pm_loss_gm'] = game
+
+            # total stats
             stats['win_more_3pa'] += home_3pa > away_3pa if home_win else away_3pa > home_3pa
             stats['win_more_3pm'] += home_3pm > away_3pm if home_win else away_3pm > home_3pm
             stats['win_more_3p_perc'] += (home_3pm / home_3pa > away_3pm / away_3pa) if home_win else (home_3pm / home_3pa < away_3pm / away_3pa)
@@ -34,22 +82,20 @@ def main(season):
             stats['win_more_ftm'] += home_ftm > away_ftm if home_win else away_ftm > home_ftm
             stats['total_games'] += 1
 
-        except:
-            print("ERROR - MISSING DATA?:", game)
+        except Exception as e:
+            print("ERROR - MISSING DATA?:", game, e.args)
 
     total_games = stats['total_games']
-    win_more_3pa = stats['win_more_3pa']
-    win_more_3pm = stats['win_more_3pm']
-    win_more_3pacc = stats['win_more_3p_perc']
-    win_more_fta = stats['win_more_fta']
-    win_more_ftm = stats['win_more_ftm']
-
-    print(f'Teams with more 3PA won {win_more_3pa/total_games*100:.2f}% of the games.')
-    print(f'Teams with more 3PM won {win_more_3pm/total_games*100:.2f}% of the games.')
-    print(f'Teams with better 3P% won {win_more_3pacc/total_games*100:.2f}% of the games.')
-    print(f'Teams with more FTA won {win_more_fta/total_games*100:.2f}% of the games.')
-    print(f'Teams with more FTM won {win_more_ftm/total_games*100:.2f}% of the games.')
-
+    
+    print(f"Teams with more 3PA won {stats['win_more_3pa']/total_games*100:.2f}% of the games.")
+    print(f"Teams with more 3PM won {stats['win_more_3pm']/total_games*100:.2f}% of the games.")
+    print(f"Teams with better 3P% won {stats['win_more_3p_perc']/total_games*100:.2f}% of the games.")
+    print(f"Teams with more FTA won {stats['win_more_fta']/total_games*100:.2f}% of the games.")
+    print(f"Teams with more FTM won {stats['win_more_ftm']/total_games*100:.2f}% of the games.")
+    print(f"Biggest point margin in a win with fewer 3PM was in {stats['biggest_win_margin_fewer_3pm_gm']['home_team']} @ {stats['biggest_win_margin_fewer_3pm_gm']['away_team']}, {stats['biggest_win_margin_fewer_3pm_gm']['home_score']}-{stats['biggest_win_margin_fewer_3pm_gm']['away_score']} ({stats['biggest_win_margin_fewer_3pm_gm']['date']}): {stats['biggest_win_margin_fewer_3pm']} points.")
+    print(f"Fewest 3PM in a win: {stats['fewest_3pm_win_gm']['home_team']} @ {stats['fewest_3pm_win_gm']['away_team']}, {stats['fewest_3pm_win_gm']['home_score']}-{stats['fewest_3pm_win_gm']['away_score']} ({stats['fewest_3pm_win_gm']['date']}) at {stats['fewest_3pm_win']} 3PM.")
+    print(f"Most 3PM in a loss: {stats['most_3pm_loss_gm']['home_team']} @ {stats['most_3pm_loss_gm']['away_team']}, {stats['most_3pm_loss_gm']['home_score']}-{stats['most_3pm_loss_gm']['away_score']} ({stats['most_3pm_loss_gm']['date']}) at {stats['most_3pm_loss']} 3PM")
+    print(f"Best 3PM margin in a loss in {stats['more_3pm_but_loss_gm']['home_team']} @ {stats['more_3pm_but_loss_gm']['away_team']}, {stats['more_3pm_but_loss_gm']['home_score']}-{stats['more_3pm_but_loss_gm']['away_score']} ({stats['more_3pm_but_loss_gm']['date']}) at {stats['more_3pm_but_loss']}. {stats['more_3pm_but_loss_gm']['home_team']} {stats['more_3pm_but_loss_gm']['detailed_home']['fg3']} vs {stats['more_3pm_but_loss_gm']['away_team']} {stats['more_3pm_but_loss_gm']['detailed_away']['fg3']}.")
 
 if __name__ == "__main__":
     main('2025')
